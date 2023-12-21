c2c7a6a9bf7a2eaf304f80d480b27975f16de899
Stefan Krsteski <58995762+Stefanstud@users.noreply.github.com>
2023-12-21 21:35:01 +0100

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -22,8 +22,12 @@ function createDollar() {
     const randomPosition = Math.random() * window.innerWidth;
     dollar.style.left = (randomPosition < windowThird || randomPosition > 2 * windowThird) ? randomPosition + 'px' : (2 * windowThird) + 'px';
     dollar.style.animationDuration = Math.random() * 3 + 2 + 's'; // Randomize fall speed
-    document.getElementById('fallingDollars').appendChild(dollar);
+    // Rotate the dollar by a random degree between 0 and 360
+    const rotationDegree = Math.random() * 360;
+    dollar.style.transform = 'rotate(' + rotationDegree + 'deg)';
 
+    document.getElementById('fallingDollars').appendChild(dollar);
+  
     // Remove dollar after it falls out of view
     setTimeout(() => {
         dollar.remove();
